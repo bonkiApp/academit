a2b42fc3949eeb176b1b6a813da6b44366f8c0f4
Admin <kbond@ngs.ru>
2016-02-25 21:07:22 +0600

    улучшение первого задания

--- JS/src/jscript.js
+++ JS/src/jscript.js
@@ -1,9 +1,11 @@
-var foo = "";
-var bar = "";
-for (var x=1; x<=10; x++) {
-    foo = "Строка " + x + "\n";
-    bar = bar + foo;
+function Strings() {
+    var foo = "";
+    var bar = "";
+    for (var x = 1; x <= 10; x++) {
+        foo = "Строка " + x + "\n";
+        bar = bar + foo;
+    }
+    alert(bar);
 }
-alert(bar);
 
 
