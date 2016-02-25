5b39b8b02f8f6eb7715a12ee6608ab7fce1123b4
Admin <kbond@ngs.ru>
2016-02-25 22:29:29 +0600

    окно ввода чисел, с проверкой

--- JS/src/jscript.js
+++ JS/src/jscript.js
@@ -9,3 +9,25 @@ function Strings() {
 }
 
 
+function Equation2(){
+    do {
+        var index1 = prompt("Введите первое число - коэффициент a:", '');
+        index1 = parseFloat(index1, 10);
+    } while (isNaN(index1));
+
+    do {
+        var index2 = prompt("Введите второе число - коэффициент b:", '');
+        index2 = parseFloat(index2, 10);
+    } while (isNaN(index2));
+
+    do {
+        var index3 = prompt("Введите третье число - коэффициент c:", '');
+        index3 = parseFloat(index3, 10);
+    } while (isNaN(index3));
+
+
+
+
+
+
+}
