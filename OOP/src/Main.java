294789992366ab0fb7e1aae3f9bd156f1fe9b17f
Admin <kbond@ngs.ru>
2016-04-24 16:56:31 +0600

    main point

---
+++ OOP/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Point point1 = new Point(5,1);
+        Point point2 = new Point(10,3);
+
+        double distanse = point1.foundDistanse(point2);
+
+        System.out.println(distanse);
+
+    }
+
+}
