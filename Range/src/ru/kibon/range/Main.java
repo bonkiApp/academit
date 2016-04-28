feccd26f593fa1d546d7a554ba1eac2992626063
Admin <kbond@ngs.ru>
2016-04-28 19:13:21 +0600

    интервалы работают

--- Range/src/ru/kibon/range/Main.java
+++ Range/src/ru/kibon/range/Main.java
@@ -34,6 +34,9 @@ public class Main {
             String joinR = newRange.joinRanges(newFrom, newTo, newFrom1, newTo1);
             System.out.println(joinR);
 
+            String subR = newRange.subtractRanges(newFrom, newTo, newFrom1, newTo1);
+            System.out.println(subR);
+
         } catch (InputMismatchException e) {
             System.out.println("Вы ввели не число, начните заново.");
         }
--- Range/src/ru/kibon/range/Range.java
+++ Range/src/ru/kibon/range/Range.java
@@ -36,8 +36,6 @@ public class Range {
 
     double x1;
     double x2;
-    double y1;
-    double y2;
     public String crossingRanges(double from, double to, double from1, double to1){
         if (from == from1 || to == to1){
             return "Диапазоны совпадают";
@@ -65,11 +63,27 @@ public class Range {
        return String.format("Интервал пересечения: (%f, %f)", x1, x2 );
     }
 
+    double y1;
+    double y2;
     public String joinRanges(double from, double to, double from1, double to1) {
         y1 = (from < from1) ? from: from1;
         y2 = (to > to1) ? to: to1;
         return String.format("Интервал объединения: (%f, %f)", y1, y2);
     }
+
+    double z1;
+    double z2;
+    public String subtractRanges (double from, double to, double from1, double to1){
+        if (x1 != 0){
+            z1 = from - to1;
+            z2 = from1 - to;
+            return String.format("Интервал разности: (%f, %f)", z1, z2);
+        } else {
+            return String.format("Нет пересечения: (%f, %f)", from, to);
+        }
+
+    }
+
 
 }
 
