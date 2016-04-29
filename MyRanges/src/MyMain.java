14a4085f4e913649f56cb508fa773c9fc8829efd
Admin <kbond@ngs.ru>
2016-04-29 17:40:04 +0600

    уже лучше

--- MyRanges/src/MyMain.java
+++ MyRanges/src/MyMain.java
@@ -1,6 +1,6 @@
 public class MyMain {
     public static void main(String[] args) {
-        MyRange range1 = new MyRange(-5.78, 42.99);
+        MyRange range1 = new MyRange(-5.78, 14.99);
         double number = 11.4;
 
         String answer = (range1.isInside(number)) ? "" : " не";
@@ -9,12 +9,15 @@ public class MyMain {
         double range = range1.findRange(range1);
         System.out.printf("Длина интервала %f%n", range);
 
-        MyRange range2 = new MyRange(-10.5, 25.1); //(-5.78, 42.99)
+        MyRange range2 = new MyRange(10.5, 25.1); //(-5.78, 42.99)
 //
         MyRange crossR = range1.crossingRanges(range2);
-        System.out.printf("Интервал пересения: %s", crossR);
+        System.out.printf("Интервал пересечения: %s%n", range1.printRange(crossR));
 
-//        System.out.println(range1.printRange(range2));
+        MyRange joinR = range1.joinRanges(range2);
+        System.out.printf("Интервал объединения: %s%n", range1.printRange(joinR));
+
+//        System.out.println(range1.printRange(crossR));
 
     }
 }
