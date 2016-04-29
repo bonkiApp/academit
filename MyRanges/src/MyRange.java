f8cae9300908d9d3c5bacc310d222eb3f21621e2
Admin <kbond@ngs.ru>
2016-04-29 10:00:48 +0600

    myrange

--- MyRanges/src/MyRange.java
+++ MyRanges/src/MyRange.java
@@ -32,28 +32,40 @@ public class MyRange {
         return rangeLength;
     }
 
-    public MyRange crossingRanges(MyRange range1, MyRange range2) {
-        double x1;
-        double x2;
-        if (from == from1 || to == to1) {
-            return "Интервалы совпадают";
-        } else if ((from < from1 && from1 < to) && (from1 < to && to < to1)) {
-            x1 = from1;
-            x2 = to;
-        } else if ((from1 < from && from < to1) && (from < to1 && to1 < to)) {
-            x1 = from;
-            x2 = to1;
-        } else if ((from1 < from && from < to1) && (from1 < to && to < to1)) {
-            x1 = from;
-            x2 = to;
-        } else if ((from < from1 && from1 < to) && (from < to1 && to1 < to)) {
-            x1 = from1;
-            x2 = to1;
-        } else {
-            return String.format("NULL");
-        }
+//    public MyRange crossingRanges(MyRange range2) {
+//        double x1;
+//        double x2;
+//        if (this.from == range2.from || this.to == range2.to) {
+//            return ((this.from, this.to));
+//        }
+//        else if ((from < from1 && from1 < to) && (from1 < to && to < to1)) {
+//            x1 = from1;
+//            x2 = to;
+//        } else if ((from1 < from && from < to1) && (from < to1 && to1 < to)) {
+//            x1 = from;
+//            x2 = to1;
+//        } else if ((from1 < from && from < to1) && (from1 < to && to < to1)) {
+//            x1 = from;
+//            x2 = to;
+//        } else if ((from < from1 && from1 < to) && (from < to1 && to1 < to)) {
+//            x1 = from1;
+//            x2 = to1;
+//        } else {
+//            return String.format("NULL");
+//        }
+//        return (this);
+//        return String.format("Интервал пересечения: (%f, %f)", x1, x2);
+//    }
 
-        return String.format("Интервал пересечения: (%f, %f)", x1, x2);
+//    public MyRange joinRanges(MyRange range2) {
+//        double y1;
+//        double y2;
+//        y1 = (this.from < from) ? this.from : from;
+//        y2 = (this.to > to) ? this.to : to;
+//        return String.format("Интервал объединения: (%f, %f)", y1, y2);
+//    }
+
+    public String printRange (MyRange range){
+        return String.format("(%f, %f)", range.from, range.to);
     }
-
 }
