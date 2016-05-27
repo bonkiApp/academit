1b742e3f671fb7ffa1245462e368f3d647bac52f
Admin <kbond@ngs.ru>
2016-05-27 21:21:33 +0600

    0

--- ru/kibon/Shape/src/Main.java
+++ ru/kibon/Shape/src/Main.java
@@ -3,12 +3,14 @@
 public class Main {
     public static void main(String[] args) {
         Square sq = new Square(4);
-        Triangle tri = new Triangle(3, 2, 3, 7, 5, 4.5);
+        Triangle tri = new Triangle(3, 2, 3, 7.2, 5, 4.5);
         Rectangle rec = new Rectangle(6, 4);
         Circle cir = new Circle(8.5);
-// сделать проверку на отрицательное, не числа, на нули, добавить исключения.
+
         System.out.println(sq.getArea());
         System.out.println(tri.getArea());
+        System.out.println(tri.getWidth());
+        System.out.println(tri.getHeight());
         System.out.println(rec.getArea());
         System.out.println(cir.getArea());
     }
