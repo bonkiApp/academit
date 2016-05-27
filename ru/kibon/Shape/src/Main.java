4d1d2ee985e84784a262dead73ae0f3cb4ee0cbb
Admin <kbond@ngs.ru>
2016-05-27 21:23:43 +0600

    название переменных

--- ru/kibon/Shape/src/Main.java
+++ ru/kibon/Shape/src/Main.java
@@ -2,16 +2,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        Square sq = new Square(4);
-        Triangle tri = new Triangle(3, 2, 3, 7.2, 5, 4.5);
-        Rectangle rec = new Rectangle(6, 4);
-        Circle cir = new Circle(8.5);
+        Square square = new Square(4);
+        Triangle triangle = new Triangle(3, 2, 3, 7.2, 5, 4.5);
+        Rectangle rectangle = new Rectangle(6, 4);
+        Circle circle = new Circle(8.5);
 
-        System.out.println(sq.getArea());
-        System.out.println(tri.getArea());
-        System.out.println(tri.getWidth());
-        System.out.println(tri.getHeight());
-        System.out.println(rec.getArea());
-        System.out.println(cir.getArea());
+        System.out.println(square.getArea());
+        System.out.println(triangle.getArea());
+        System.out.println(triangle.getWidth());
+        System.out.println(triangle.getHeight());
+        System.out.println(rectangle.getArea());
+        System.out.println(circle.getArea());
     }
 }
