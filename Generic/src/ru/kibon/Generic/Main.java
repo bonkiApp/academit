5b3b90d55210f9e039469a6d162249e4a2fa527b
Admin <kbond@ngs.ru>
2016-05-28 01:28:11 +0600

    generic

--- Generic/src/ru/kibon/Generic/Main.java
+++ Generic/src/ru/kibon/Generic/Main.java
@@ -2,7 +2,12 @@ package ru.kibon.Generic;
 
 public class Main {
     public static void main(String[] args) {
-        Pair<Integer, Integer> pair1 = new Pair<>(14, 55);
+        Pair<String, Double> pair1 = new Pair<>("14", 57.1 - 1);
+        Pair<String, Double> pair2 = new Pair<>("14", 56.1000001);
+
         System.out.println(pair1.toString());
+
+        boolean result = Util.compare(pair1, pair2);
+        System.out.println(result);
     }
 }
--- Generic/src/ru/kibon/Generic/Util.java
+++ Generic/src/ru/kibon/Generic/Util.java
@@ -1,7 +1,12 @@
 package ru.kibon.Generic;
 
 public class Util {
-    public static <K, V> boolean compare (Pair<K, V> p1, Pair <K, V> p2){
-        return p1.getFirst().equals(p2.getFirst()) && p1.getSecond().equals(p2.getSecond());
+    public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+
+        if (p1.getFirst().hashCode() != p2.getFirst().hashCode() || p1.getSecond().hashCode() != p2.getSecond().hashCode()) {
+            return false;
+        } else {
+            return p1.getFirst().equals(p2.getFirst()) && p1.getSecond().equals(p2.getSecond());
+        }
     }
 }
