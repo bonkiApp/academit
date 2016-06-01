56ce38caf372daf1504969f23ed2e1f9d7bac8e9
Admin <kbond@ngs.ru>
2016-06-01 22:06:21 +0600

    исправления

--- Generic/src/ru/kibon/Generic/Util.java
+++ Generic/src/ru/kibon/Generic/Util.java
@@ -1,12 +1,9 @@
-package ru.kibon.Generic;
+package ru.kibon.generic;
 
 public class Util {
-    public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
-
-        if (p1.getFirst().hashCode() != p2.getFirst().hashCode() || p1.getSecond().hashCode() != p2.getSecond().hashCode()) {
-            return false;
-        } else {
-            return p1.getFirst().equals(p2.getFirst()) && p1.getSecond().equals(p2.getSecond());
-        }
+    public static <K, V> boolean equals(Pair<K, V> p1, Pair<K, V> p2) {
+        if (p1.getFirst() == p2.getFirst() && p1.getSecond() == p2.getSecond()) {
+            return true;
+        } else return false;
     }
 }
