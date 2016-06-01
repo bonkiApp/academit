ba27b6cfa28f67340e23ae6e5acde327d4f58c54
Admin <kbond@ngs.ru>
2016-06-01 22:06:03 +0600

    исправления

--- Generic/src/ru/kibon/Generic/Main.java
+++ Generic/src/ru/kibon/Generic/Main.java
@@ -1,13 +1,11 @@
-package ru.kibon.Generic;
+package ru.kibon.generic;
 
 public class Main {
     public static void main(String[] args) {
-        Pair<String, Double> pair1 = new Pair<>("14", 57.1 - 1);
-        Pair<String, Double> pair2 = new Pair<>("14", 56.1000001);
+        Pair<String, Integer> pair1 = new Pair<>("df", null);
+        Pair<String, Integer> pair2 = new Pair<>("df", null);
 
-        System.out.println(pair1.toString());
-
-        boolean result = Util.compare(pair1, pair2);
+        boolean result = Util.equals(pair1, pair2);
         System.out.println(result);
     }
 }
