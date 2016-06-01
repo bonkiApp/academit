c6f0a6beb765afcc091d91701baa4907c1b15cbd
Admin <kbond@ngs.ru>
2016-06-01 22:36:41 +0600

    еще исправления

--- Generic/src/ru/kibon/Generic/Main.java
+++ Generic/src/ru/kibon/Generic/Main.java
@@ -2,8 +2,8 @@ package ru.kibon.generic;
 
 public class Main {
     public static void main(String[] args) {
-        Pair<String, Integer> pair1 = new Pair<>("hi", null);
-        Pair<String, Integer> pair2 = new Pair<>("hi", null);
+        Pair<String, Integer> pair1 = new Pair<>("javac", null);
+        Pair<String, Integer> pair2 = new Pair<>("java", null);
 
         boolean result = Util.equals(pair1, pair2);
         System.out.println(result);
--- Generic/src/ru/kibon/Generic/Util.java
+++ Generic/src/ru/kibon/Generic/Util.java
@@ -2,6 +2,10 @@ package ru.kibon.generic;
 
 public class Util {
     public static <K, V> boolean equals(Pair<K, V> p1, Pair<K, V> p2) {
-        return p1.getFirst() == p2.getFirst() && p1.getSecond() == p2.getSecond();
+        if (p1 != null && p2 != null) {
+            return p1.getFirst() == p2.getFirst() && p1.getSecond() == p2.getSecond();
+        } else {
+            return false;
+        }
     }
 }
