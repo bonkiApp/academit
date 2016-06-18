714510bdb7578682f0594549960e1c78cbb17633
Admin <kbond@ngs.ru>
2016-06-18 18:26:49 +0600

    main

--- Vector/src/ru/kibon/vector/Main.java
+++ Vector/src/ru/kibon/vector/Main.java
@@ -2,11 +2,11 @@ package ru.kibon.vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(10);
+        Vector vector = new Vector(5);
         System.out.println(vector.toString(vector));
 
-//        Vector newVector = new Vector(vector);
-//        System.out.println(newVector);
+        Vector newVector = new Vector(vector);
+        System.out.println(newVector.toString(newVector));
 
     }
 }
