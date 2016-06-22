288b6dfc4b35b0b9e7fa5406af7398a215300a82
Admin <kbond@ngs.ru>
2016-06-22 23:26:24 +0600

    исправление ошибок

--- Vector/src/ru/kibon/vector/Main.java
+++ Vector/src/ru/kibon/vector/Main.java
@@ -3,12 +3,12 @@ package ru.kibon.vector;
 public class Main {
     public static void main(String[] args) {
 
-        int dim = 5;
+        int dim = 6;
         Vector vector = new Vector(dim);
         System.out.printf("Создание нулевого вектора размерностью %s: %s%n", dim, vector.toString(vector));
 
-        double[] compon = {2, 5, 1.5, -2, 1};
-        Vector arrVector = new Vector(dim, compon);
+        double[] component = {2, 5, 1.5, -2, 1};
+        Vector arrVector = new Vector(dim, component);
         System.out.printf("Заполнение вектора размерностью %s значениями из массива: %s%n", dim, arrVector.toString(arrVector));
 
         Vector copyArrVector = new Vector(arrVector);
@@ -18,7 +18,8 @@ public class Main {
 
         System.out.printf("Размерность вектора %s равна %s%n", copyArrVector.toString(copyArrVector), copyArrVector.getSize());
 
-        Vector vector2 = new Vector(4, new double[]{3, 2, 14.2, 1}); // дополнительный вектор для примеров
+        Vector vector2 = new Vector(5, new double[]{3, 2, 14.2, 1}); // дополнительный вектор для примеров
+        System.out.println(vector2.toString(vector2));
 
         System.out.printf("Сумма векторов %s и %s равна: %s%n", arrVector.toString(arrVector), vector2.toString(vector2), arrVector.toString(arrVector.sumVectors(vector2)));
 
@@ -27,17 +28,17 @@ public class Main {
         int scalar = 3;
         System.out.printf("Вектор %s умноженный на скаляр %s равен: %s%n", vector2.toString(vector2), scalar, vector2.toString(vector2.multipleVectors(scalar)));
 
-        vector2 = vector2.reversVector();
+        vector2 = vector2.reverseVector();
         System.out.printf("Разворот предыдущего вектора: %s%n", vector2.toString(vector2));
 
         System.out.printf("Длина вектора %s равна: %s%n", vector2.toString(vector2), vector2.calcLength());
 
-        int component = 3;
-        System.out.printf("Компонента вектора под индексом %s равна: %s%n", component, vector2.getComponent(component));
+        int component2 = 3;
+        System.out.printf("Компонента вектора под индексом %s равна: %s%n", component2, vector2.getComponent(component2));
 
         int value = 4;
-        vector2.setComponent(component, value);
-        System.out.printf("Установка значения компонента под индексом %s равным %s: %s%n", component, value, vector2.toString(vector2));
+        vector2.setComponent(component2, value);
+        System.out.printf("Установка значения компонента под индексом %s равным %s: %s%n", component2, value, vector2.toString(vector2));
 
         Vector v1 = new Vector(4, new double[]{1, 2, 3, 4});
         Vector v2 = new Vector(3, new double[]{1, 2, 3});
