bca2680c7b9508b13440719803ca5db458df5f5d
Admin <kbond@ngs.ru>
2016-06-30 22:36:22 +0600

    Матрица

---
+++ Matrix/src/ru/kibon/matrix/MainMatrix.java
@@ -0,0 +1,5 @@
+package ru.kibon.matrix;
+
+public class MainMatrix {
+
+}
--- Matrix/src/ru/kibon/matrix/Matrix.java
+++ Matrix/src/ru/kibon/matrix/Matrix.java
@@ -1,4 +1,15 @@
 package ru.kibon.matrix;
 
+import ru.kibon.vector.Vector;
+
 public class Matrix {
+    private Vector[] rows;
+
+    public Matrix(int n, int m){
+        for(int i = 0; i < n; i++){
+            Vector row[i] = new Vector(m);
+
+        }
+    }
+
 }
