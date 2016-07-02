500fbe92ce74934cf742672517e7f64dd2ea69db
Admin <kbond@ngs.ru>
2016-07-02 17:53:57 +0600

    Матрица

--- Matrix/src/ru/kibon/matrix/MainMatrix.java
+++ Matrix/src/ru/kibon/matrix/MainMatrix.java
@@ -1,5 +1,10 @@
 package ru.kibon.matrix;
 
 public class MainMatrix {
+    public static void main(String[] args) {
+        Matrix neo = new Matrix(2, 3);
+        System.out.println(neo);
 
+
+    }
 }
--- Matrix/src/ru/kibon/matrix/Matrix.java
+++ Matrix/src/ru/kibon/matrix/Matrix.java
@@ -5,10 +5,21 @@ import ru.kibon.vector.Vector;
 public class Matrix {
     private Vector[] rows;
 
-    public Matrix(int n, int m){
-        for(int i = 0; i < n; i++){
-            Vector row[i] = new Vector(m);
+    public Matrix(int n, int m) {
+        Vector[] rows = new Vector[n];
+        for (int i = 0; i < n; i++) {
+            rows[i] = new Vector(m);
+        }
+    }
 
+    public Matrix(Matrix other) {
+        this.rows = other.rows;
+        System.arraycopy(other.rows, 0, this.rows, 0, other.rows.length);
+    }
+
+    public Matrix(double[][] array) {
+        for (int i = 0; i < array.length; i++){
+            rows[i] = new Vector(array[i].length, array[i]);
         }
     }
 
