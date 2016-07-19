84e72984cd10e365b28346018b426acadd17d43b
Admin <kbond@ngs.ru>
2016-07-19 22:54:30 +0600

    доработки исправления

--- Lambda/src/ru/kibon/lambda/mainLambda.java
+++ Lambda/src/ru/kibon/lambda/mainLambda.java
@@ -1,6 +1,7 @@
 package ru.kibon.lambda;
 
 import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,26 +20,28 @@ public class mainLambda {
                 .map(n -> n.getName())
                 .distinct()
                 .collect(Collectors.joining(", ", "Имена: ", "."));
-
         System.out.println(disNames);
 
+        System.out.print("Средний возраст людей младше 18 лет: ");
         persons.stream()
                 .filter(p -> p.getAge() < 18)
                 .mapToInt(x -> x.getAge())
                 .average()
                 .ifPresent(System.out::println);
 
-        Map<String, List<Person>> persByName = persons.stream()
-                .collect(Collectors.groupingBy(p -> p.getName())); //средний возраст тезок
+        System.out.println("Средний возраст людей с одинаковым именем:");
+        Map<String, IntSummaryStatistics> persByName = persons.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.summarizingInt(Person::getAge)));
+        for (Map.Entry<String, IntSummaryStatistics> item : persByName.entrySet()) {
+            System.out.println(item.getKey() + " - " + item.getValue().getAverage());
+        }
 
-        System.out.println(persByName);
-
+        System.out.println("Люди в возрасте от 45 до 20:");
         persons.stream()
                 .filter(p -> p.getAge() > 20 && p.getAge() < 45)
                 .sorted((Person p1, Person p2) -> p2.getAge() - p1.getAge())
                 .map(n -> n.getName())
                 .forEach(System.out::println);
-
     }
 }
 
