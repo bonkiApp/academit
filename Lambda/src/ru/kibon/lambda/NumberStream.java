44783271781ea2772dbfd8840b8a9a74a3f41e82
Admin <kbond@ngs.ru>
2016-07-21 20:37:46 +0600

    исправления

--- Lambda/src/ru/kibon/lambda/NumberStream.java
+++ Lambda/src/ru/kibon/lambda/NumberStream.java
@@ -7,12 +7,11 @@ public class NumberStream {
     public static void main(String[] args) {
         System.out.println("Введите количество элементов для вычисления корней.");
         Scanner scanner = new Scanner(System.in);
-        int n;
         if (scanner.hasNextInt()) {
-            n = scanner.nextInt();
+            int n = scanner.nextInt();
             Stream.iterate(1.0, x -> x + 1)
-                    .limit(n)
                     .map(Math::sqrt)
+                    .limit(n)
                     .forEach(System.out::println);
         } else {
             System.out.println("Вы ввели не целое число.");
